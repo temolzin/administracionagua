a39bd560634620da28d216814ee8f70c627c94ac
Carmen <carmenlopezsanjuan@gmail.com>
2024-10-14 09:29:55 -0600

    permissions were changed and fixed

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -15,62 +15,60 @@ class RolesAndPermissionsSeeder extends Seeder
       
         Permission::create([
             'name' => 'viewUser',
-            'description' => 'Permite ver los usuario.'
+            'description' => 'Permite ver, editar y eliminar los administradores.'
         ])->assignRole($roleAdmin);
         Permission::create([
             'name' => 'viewRoles',
-            'description' => 'Permite ver los roles.'
+            'description' => 'Permite ver y editar los roles.'
         ])->assignRole($roleAdmin);
         Permission::create([
             'name' => 'viewCustomers',
             'description' => 'Permite ver los administradores.'
         ])->assignRole([$roleAdmin, $roleSecretariat ]);
         Permission::create([
+            'name' => 'editCustomer',
+            'description' => 'Permite editar los usuarios.'
+        ])->assignRole([$roleAdmin]);
+        Permission::create([
+            'name' => 'deleteCustomer',
+            'description' => 'Permite eliminar los usuarios.'
+        ])->assignRole([$roleAdmin ]);
+        Permission::create([
             'name' => 'viewPayments',
             'description' => 'Permite ver los pagos.'
         ])->assignRole([$roleAdmin, $roleSecretariat ]);
         Permission::create([
-            'name' => 'viewDebts',
-            'description' => 'Permite ver las deudas.'
-        ])->assignRole([$roleAdmin, $roleSecretariat ]);
-        Permission::create([
-            'name' => 'viewCost',
-            'description' => 'Permite ver los Costos.'
-        ])->assignRole([$roleAdmin, $roleSecretariat ]);
-        Permission::create([
-            'name' => 'deleteCost',
-            'description' => 'Permite eliminar los costos.'
-        ])->assignRole([$roleAdmin]);
-        Permission::create([
-            'name' => 'editCost',
-            'description' => 'Permite editar los costos.'
+            'name' => 'editPayment',
+            'description' => 'Permite editar los pagos.'
         ])->assignRole([$roleAdmin]);
         Permission::create([
             'name' => 'deletePayment',
             'description' => 'Permite eliminar los pagos.'
         ])->assignRole([$roleAdmin]);
         Permission::create([
-            'name' => 'editPayment',
-            'description' => 'Permite editar los pagos.'
-        ])->assignRole([$roleAdmin]);
-        Permission::create([
-            'name' => 'deleteDebt',
-            'description' => 'Permite eliminar la deuda.'
-        ])->assignRole([$roleAdmin]);
+            'name' => 'viewDebts',
+            'description' => 'Permite ver las deudas.'
+        ])->assignRole([$roleAdmin, $roleSecretariat ]);
         Permission::create([
             'name' => 'editDebts',
             'description' => 'Permite editar la deuda.'
         ])->assignRole([$roleAdmin ]);
         Permission::create([
-            'name' => 'editCustomer',
-            'description' => 'Permite editar los administradores.'
-        ])->assignRole([$roleAdmin ]);
+            'name' => 'deleteDebt',
+            'description' => 'Permite eliminar la deuda.'
+        ])->assignRole([$roleAdmin]);
         Permission::create([
-            'name' => 'deleteCustomer',
-            'description' => 'Permite eliminar los administradores.'
-        ])->assignRole([$roleAdmin ]);
-        
-
+            'name' => 'viewCost',
+            'description' => 'Permite ver los Costos.'
+        ])->assignRole([$roleAdmin, $roleSecretariat ]);
+        Permission::create([
+            'name' => 'editCost',
+            'description' => 'Permite editar los costos.'
+        ])->assignRole([$roleAdmin]);
+        Permission::create([
+            'name' => 'deleteCost',
+            'description' => 'Permite eliminar los costos.'
+        ])->assignRole([$roleAdmin]);
     }
 }
 
