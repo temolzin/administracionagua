49478bc561bd2023950d309b2c54e151057720e0
Carmen <carmenlopezsanjuan@gmail.com>
2024-11-13 11:32:40 -0600

    debt alerts were added

--- resources/views/customers/customerWhitDebs.blade.php
+++ resources/views/customers/customerWhitDebs.blade.php
@@ -1,18 +1,18 @@
-<div class="modal fade" id="allCustomersModal" tabindex="-1" role="dialog" aria-labelledby="allCustomersModalLabel" aria-hidden="true">
+<div class="modal fade" id="debtCustomersModal" tabindex="-1" role="dialog" aria-labelledby="debtCustomersModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
-            <div class="card-header" style="background-color:#9b1010; color:white;">
-                <h5 class="modal-title" id="allCustomersModalLabel">Clientes con Deuda Mayor a 3 Años</h5>
+            <div class="card-header" id="modalHeader" style="color:white;">
+                <h5 class="modal-title" id="debtCustomersModalLabel">Clientes con Deuda</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <table id="customersWhitDebts" class="table table-striped display responsive nowrap" style="width:100%">
+                <table id="debtCustomersTable" class="table table-striped display responsive nowrap" style="width:100%">
                     <thead>
                         <tr>
+                            <th>ID</th>
                             <th>Nombre</th>
-                            <th>Apellido</th>
                             <th>Meses de Deuda</th>
                         </tr>
                     </thead>
