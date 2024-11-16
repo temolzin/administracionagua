d98c3d2bb89fadd57e82012e03e33b106c0ded93
Carmen <carmenlopezsanjuan@gmail.com>
2024-11-15 18:57:21 -0600

    A backup library was installed to have backups of the database and projects

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,8 +15,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('backup:clean')->everyMinute();
     }
+
 
     /**
      * Register the commands for the application.
--- config/filesystems.php
+++ config/filesystems.php
@@ -53,6 +53,11 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
 
+        'respaldoBD' => [
+        'driver' => 'local',
+        'root' => 'C:\\Users\\carme\\OneDrive\\Escritorio\\respaldoBD',
+    ],
+
     ],
 
     /*
